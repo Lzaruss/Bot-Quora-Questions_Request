1f37d9dfe7e9982923559b025f4a707fad77cce2
Alberto Lazaro <104428151+lzaruss@users.noreply.github.com>
2022-12-29 15:02:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -55,20 +55,6 @@ def get_question():
 
 def return_questions():
   
-  HEADERS = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
-      "Accept":"*/*",
-      "Keep-Alive": "300",
-      "Connection": "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-  }
   # Toma el tiempo de inicio
   start_time = time.perf_counter()
   options = webdriver.ChromeOptions() 
